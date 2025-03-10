3087311e4755becece95b97c1417d8dd93909a82
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-03-09 22:47:44 -0500

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -3,23 +3,28 @@ const Registro = require('../models/registro');
 const moment = require('moment-timezone');
 const router = express.Router();
 
-// Ruta para registrar entrada
+// Función para obtener la fecha en la zona horaria de Bogotá
+const obtenerFechaLocal = () => moment.tz("America/Bogota").subtract(5, 'hours').toDate();
+
+// Ruta para registrar entrada con imagen
 router.post('/entrada', async (req, res) => {
-    // Incluimos "cedula" en la desestructuración
-    const { userId, deviceID, cedula, ubicacion } = req.body;
+    const { userId, deviceID, cedula, ubicacion, imagenBase64 } = req.body;
 
     try {
-        // Establece la fecha y hora actual en la zona horaria de Bogotá y ajusta la conversión a UTC
-        let fechaLocal = moment.tz("America/Bogota").subtract(5, 'hours');
+        if (!imagenBase64) {
+            return res.status(400).json({ msg: 'No se recibió ninguna imagen' });
+        }
 
         const nuevoRegistro = new Registro({
             userId,
             deviceID,
-            cedula, // Guardamos la cédula
+            cedula,
             ubicacion,
-            fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
-            tipo: 'entrada'
+            fecha: obtenerFechaLocal(),
+            tipo: 'entrada',
+            imagenBase64 // Guardamos la imagen en Base64
         });
+
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Entrada registrada exitosamente' });
     } catch (error) {
@@ -28,23 +33,25 @@ router.post('/entrada', async (req, res) => {
     }
 });
 
-// Ruta para registrar salida
+// Ruta para registrar salida con imagen
 router.post('/salida', async (req, res) => {
-    // Incluimos "cedula" en la desestructuración
-    const { userId, deviceID, cedula, ubicacion } = req.body;
+    const { userId, deviceID, cedula, ubicacion, imagenBase64 } = req.body;
 
     try {
-        // Establece la fecha y hora actual en la zona horaria de Bogotá y ajusta la conversión a UTC
-        let fechaLocal = moment.tz("America/Bogota").subtract(5, 'hours');
+        if (!imagenBase64) {
+            return res.status(400).json({ msg: 'No se recibió ninguna imagen' });
+        }
 
         const nuevoRegistro = new Registro({
             userId,
             deviceID,
-            cedula, // Guardamos la cédula
+            cedula,
             ubicacion,
-            fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
-            tipo: 'salida'
+            fecha: obtenerFechaLocal(),
+            tipo: 'salida',
+            imagenBase64 // Guardamos la imagen en Base64
         });
+
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Salida registrada exitosamente' });
     } catch (error) {
