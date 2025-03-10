8bb0ed80e485d512ad4db5e27213a2c7f31a0071
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-03-09 22:48:08 -0500

    Update registro.js

--- models/registro.js
+++ models/registro.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const registroSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     deviceID: { type: String, required: true },
-    cedula: { type: String, required: true }, // Campo agregado para la cédula
+    cedula: { type: String, required: true }, 
     ubicacion: {
         lat: { type: Number, required: true },
         lng: { type: Number, required: true }
     },
-    fecha: { type: Date, default: Date.now }, // Se sobrescribe con obtenerFechaLocal()
-    tipo: { type: String, required: true } // 'entrada' o 'salida'
+    fecha: { type: Date, default: Date.now }, 
+    tipo: { type: String, required: true }, // 'entrada' o 'salida'
+    imagenBase64: { type: String, required: true } // Nuevo campo para almacenar la imagen en Base64
 });
 
 module.exports = mongoose.model('Registro', registroSchema);
